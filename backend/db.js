df5951c87ee000dd2b861ff1cbfa0f865a7ea2c8
kali <kali@kali>
2023-05-07 11:48:47 -0400

    authtoken and paper salt password hashing

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 
 const mongooses=require('mongoose')
-const mongoURI='mongodb://127.0.0.1:27017/test'
+const mongoURI='mongodb://127.0.0.1:27017/notebook'
 
 const connectToMongo =()=>{
     mongooses.connect(mongoURI,console.log('connected to mongo'))
